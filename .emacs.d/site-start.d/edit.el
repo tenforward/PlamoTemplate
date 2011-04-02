5de25983a2cb0c5879fd373102e3747ff2713c7d
KATOH Yasufumi <karma@jazz.email.ne.jp>
2011-04-02 14:40:30 +0900

    Add setting for assigning C-h to BS

--- .emacs.d/site-start.d/edit.el
+++ .emacs.d/site-start.d/edit.el
@@ -3,7 +3,7 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 ;;; 最終更新日の自動挿入
-;;;   ファイルの先頭から 8 行以内に Time-stamp: <> または
+;;;   ファイルの先頭から 8 行以内に Time-stamp: <2011-04-02 14:40:21 karma> または
 ;;;   Time-stamp: " " と書いてあれば、セーブ時に自動的に日付が挿入されます
 (add-hook 'write-file-hooks 'time-stamp)
 
@@ -37,6 +37,9 @@
 
 ;;; 長い行を折り返して表示しない
 ;(set-default 'truncate-lines t)
+
+;;; Ctrl-h を BS に
+(global-set-key (kbd "C-h") 'delete-backward-char)
 
 ;;; タブを使わない
 (setq-default indent-tabs-mode nil)
