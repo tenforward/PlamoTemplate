f256bfbd9c9fec27aabfd095549496941d4b0830
KATOH Yasufumi <karma@jazz.email.ne.jp>
2011-11-07 14:42:30 +0900

    ibus.el設定の無効化と設定の記述の変更
      * ibus.el は Plamo デフォルトではインストールされないので，全設定をコメントアウト
      * ibus のトグルキーの設定の記述を新しめの記述にした

--- .emacs.d/site-start.d/ibus-el.el
+++ .emacs.d/site-start.d/ibus-el.el
@@ -1,11 +1,14 @@
 ;;; ibus.el 設定 (contrib/emacs 以下にあります)
 ;;;   http://www11.atwiki.jp/s-irie/pages/21.html
-(require 'ibus)
-(add-hook 'after-init-hook 'ibus-mode-on)
+;(require 'ibus)
+;(add-hook 'after-init-hook 'ibus-mode-on)
 
 ;; Ctrl-Space は Set Mark に使用
-(ibus-define-common-key ?\C-\s nil)
-;; 
-(setq ibus-cursor-color '("red" "blue" "limegreen"))
-;; 
-(global-set-key "\C-\\" 'ibus-toggle)
+;(ibus-define-common-key ?\C-\s nil)
+;; ibus オン・オフ・無効化時のカーソル色を変更する
+;(setq ibus-cursor-color '("red" "blue" "limegreen"))
+;; ibus のトグルキーの設定
+;;; Ctrl-\
+;(global-set-key (kbd "C-\\") 'ibus-toggle)
+;;; Meta-Space
+;(global-set-key (kbd "M-SPC") 'ibus-toggle)
